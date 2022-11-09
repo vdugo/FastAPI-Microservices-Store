17bb1b2e60830aeec51f81cdbd98faf9b6a14026
Vincent <v3rdugo@yahoo.com>
2022-11-08 23:56:53 -0800

    create ProductOrder and Order models, route for creating orders and saving it into the redis db

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+import requests
 
 load_dotenv()
 
@@ -22,3 +23,39 @@ redis = get_redis_connection(
     password=os.getenv('REDIS_PASSWORD'),
     decode_responses=True
 )
+
+class ProductOrder(HashModel):
+    product_id: str
+    quantity: int
+    
+    class Meta():
+        database = redis
+        
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+    
+    class Meta():
+        database = redis
+        
+@app.post('/orders')
+def create(productOrder: ProductOrder):
+    req = requests.get('http://localhost:8000/product/{productOrder.product_id}')
+    product = req.json()
+    fee = product['price'] * 0.2
+    
+    order = Order(
+        product_id = productOrder.product_id,
+        price = product['price'],
+        fee = fee,
+        total = product['price'] + fee,
+        quantity = productOrder.quantity,
+        status = 'pending'
+    )
+    
+    return order.save()
+    
